05804adae329898d989c287d118d1d1d6c7fcd8d
zdarova1 <34815465+zdarova1@users.noreply.github.com>
2023-06-13 03:24:07 +0300

    Add files via upload

--- project_OC/src/client.py
+++ project_OC/src/client.py
@@ -1,12 +1,13 @@
 import socket
 from encoding import useKey
 from time import sleep
-
+import customtkinter as ctk
+from os.path import basename
 #параметры
-#host = 'localhost'
-#port = 1234
+host = '192.168.1.17'
+port = 1234
 buff_size = 1024
-#private_key, public_key = None, None
+private_key, public_key = None, None
 
 
 
@@ -44,55 +45,62 @@ def recieveFile(client, file_name):
 
 def ServerMeet(socket_, command):
     #command = input()
-    while command != 'exit':
-        s = command.split(' ')
-        if s[0] == 'send_file':
-            socket_.send(command.encode())
-            file_name = s[1]
-            sendFile(socket_, file_name)
-
-        elif s[0] == 'use_key':
-            socket_.send(command.encode())
-            with open(s[1], 'rb') as f:
-                data = f.read(1024)
-            socket_.send(data)
-            #private_key = useKey(s[1])
-            #socket_.send(private_key.encode())
-        elif s[0] == 'encode_file':
-            socket_.send(command.encode())  
-
-        elif s[0] == 'verify_sign':
-            socket_.send(command.encode())
-
-        elif s[0] == 'send_encode_file': #send_encode_file <path>
-            socket_.send(command.encode())
-            file_name = s[1]
-            sendFile(socket_, file_name)
-            recieveFile(socket_, s[1]+'.sig')
-        elif s[0] == 'send_encode_file_with_key': #send_encode_file <path> <key_path>
-            file_name = s[1]
-            socket_.send(command.encode())
-            #socket_.send(file_name.encode())
-            sendFile(socket_, file_name)
-            #time.sleep(1)
-            with open(s[2], 'rb') as f:
-                data = f.read(1024)
-            socket_.send(data)
-            #time.sleep(1)
-            sig_name = socket_.recv(buff_size).decode()
-            print(f'Rec sig_name: {sig_name}')
-            recieveFile(socket_, sig_name)
-
-        elif s[0] == 'rec_file':
-            socket_.send(command.encode())
-            file_name = s[1]
-            recieveFile(socket_, file_name)
-
-
-        command = input()
-    socket_.send(command.encode())
-    socket_.close()
+    
+    #while command != 'exit':
+    s = command.split(' ')
+    if s[0] == 'send_file':
+       # filepath = ctk.filedialog.askopenfilename()
+       # print(filepath)
+        socket_.send((s[0]+ ' ' +basename(s[1])).encode())
+        file_name = s[1]
+        sendFile(socket_, file_name)
+    elif s[0] == 'use_key':
+        socket_.send(command.encode())
+        with open(s[1], 'rb') as f:
+            data = f.read(1024)
+        socket_.send(data)
+        #private_key = useKey(s[1])
+        #socket_.send(private_key.encode())
+    elif s[0] == 'use_key_pub':
+        socket_.send(command.encode())
+        with open(s[1], 'rb') as f:
+            data = f.read(1024)
+        socket_.send(data)
+    elif s[0] == 'encode_file':
+        socket_.send(command.encode())  
+        recieveFile(socket_, s[1]+'.sig')
+        
+    elif s[0] == 'verify_sign':
+        socket_.send(command.encode())
+        return socket_.recv(buff_size).decode()
+    elif s[0] == 'send_encode_file': #send_encode_file <path>
+        socket_.send(command.encode())
+        file_name = s[1]
+        sendFile(socket_, file_name)
+        recieveFile(socket_, s[1]+'.sig')
+    elif s[0] == 'send_encode_file_with_key': #send_encode_file <path> <key_path>
+        file_name = s[1]
+        socket_.send(command.encode())
+        #socket_.send(file_name.encode())
+        sendFile(socket_, file_name)
+        #time.sleep(1)
+        with open(s[2], 'rb') as f:
+            data = f.read(1024)
+        socket_.send(data)
+        #time.sleep(1)
+        sig_name = socket_.recv(buff_size).decode()
+        print(f'Rec sig_name: {sig_name}')
+        recieveFile(socket_, sig_name)
+    elif s[0] == 'rec_file':
+        socket_.send(command.encode())
+        file_name = s[1]
+        recieveFile(socket_, file_name)
+        #command = input()
+    elif s[0] == 'exit':
+        socket_.send(command.encode())
+    return None
+    #socket_.close()
 #socket_ = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 #socket_.connect((host, port))
 
-#ServerMeet(socket_)
+#ServerMeet(socket_, '')
--- project_OC/src/gui.py
+++ project_OC/src/gui.py
@@ -3,11 +3,12 @@ import customtkinter as ctk
 from PIL import Image
 from encoding import useKey, EncodeFile, VerifyFile, UsePub
 from os import getcwd
+from os.path import basename
 import socket
 from client import sendFile, recieveFile, ServerMeet
 
 #параметры
-host = 'localhost'
+host = '192.168.1.17'
 port = 1234
 buff_size = 1024
 
@@ -17,7 +18,8 @@ class App(ctk.CTk):
         
         self.cur_dir = getcwd()
 
-
+        self.resizable(1,1)
+        
         self.private_key = None
         self.public_key = None
 
@@ -55,18 +57,20 @@ class App(ctk.CTk):
         self.lablekey_image = ctk.CTkImage(Image.open('images\\key2.png'), size = (400, 400))
         self.lableverify_image1 = ctk.CTkImage(Image.open('images\\cross.png'), size = (400, 400))
         self.lableverify_image2 = ctk.CTkImage(Image.open('images\\verify.png'), size = (400, 400))
-        
+        self.table_image = ctk.CTkImage(Image.open('images\\table.png'), size = (512, 512))
         #frame
         self.navigation_frame = ctk.CTkFrame(self, corner_radius=0, width=400)
-        self.navigation_frame.grid(row=0, column=0, sticky='nsew')
+        self.navigation_frame.grid(row=0, column=0, sticky='nsew', padx=10, pady=10)
        # self.navigation_frame.grid_rowconfigure(2, weight=1)
        
         #lable
-        self.lable_logo = ctk.CTkLabel(self.navigation_frame, corner_radius=0, image=logo_image, font=ctk.CTkFont(size=20, weight="bold"), compound='left', text='signify')
+        self.lable_logo = ctk.CTkLabel(self.navigation_frame, corner_radius=0, image=logo_image, font=ctk.CTkFont(size=20, weight="bold"), compound='left', text='signify      ')
         self.lable_logo.grid(row=0, column = 0, padx = 20, pady = 15)
+        
+        
         #buttons        
         self.add_button = ctk.CTkButton(self.navigation_frame, text=' upload file', image=add_image, compound='left', height=40, fg_color='#C57A44')
-        self.add_button.grid(row=1, column = 0, sticky = 'ew', padx=10, pady=5)
+        self.add_button.grid(row=1, column = 0, sticky = 'nsew', padx=10, pady=5)
 
         self.verify = ctk.CTkButton(self.navigation_frame, text=' verify sign', image=verify_image, compound='left', height=40, fg_color='#C57A44', command=self.verify_main)
         self.verify.grid(row=2, column = 0, sticky = 'ew', padx=10, pady=5)
@@ -78,10 +82,16 @@ class App(ctk.CTk):
         self.main_frame = ctk.CTkFrame(self, corner_radius=0)
       #  self.main_frame.grid_columnconfigure(0, weight=1)
         self.main_frame.grid(row=0, column=1, padx=10, pady = 10, sticky = 'nsew')
+        
+        self.label = ctk.CTkLabel(self.main_frame, corner_radius=0, image=self.table_image, font=ctk.CTkFont(size=20, weight="bold"), compound='left', text='')
+        self.label.grid(row=0,column=1, padx=25, pady = 25)
 
         
         #buttons for verify page
        # self.button_verify = ctk.CTkButton(self.main_frame, )
+    def __del__(self):
+        self.client.send('exit'.encode())
+        self.client.close()
 
     def encode_main(self):
         for widget in self.main_frame.winfo_children():
@@ -91,24 +101,27 @@ class App(ctk.CTk):
         #Label 
         self.label = ctk.CTkLabel(self.main_frame, corner_radius=0, text='Выберите файл и ключ для шифрования:', 
                                   image=self.lablekey_image, compound='top', anchor=ctk.CENTER,font=ctk.CTkFont(size=20, weight="bold"))
-        self.label.grid(row=0, column=1)
+        self.label.grid(row=0, column=2)
+        
+        
         #buttons for encode page
-        self.button_file = ctk.CTkButton(self.main_frame, corner_radius=0, text = 'Private key', fg_color='#C57A44', command=lambda: self.pick_file(('Pem Files', '*.pem'), 'private_key'))
-        self.privateKey_file = ctk.CTkButton(self.main_frame, corner_radius=0, text = 'Signing file', fg_color='#C57A44', command=lambda: self.pick_file(('All files', '*.*'), 'encode_file_path'))
+        self.button_file = ctk.CTkButton(self.main_frame, corner_radius=0, text = 'Private key', fg_color='#C57A44', 
+                                         command=lambda: self.pick_file(('Pem Files', '*.pem'), 'private_key'))
+        self.privateKey_file = ctk.CTkButton(self.main_frame, corner_radius=0, text = 'Signing file', fg_color='#C57A44', 
+                                             command=lambda: self.pick_file(('All files', '*.*'), 'encode_file_path'))
         
         #self.sign_file = ctk.CTkButton(self.main_frame, corner_radius=0, text = 'Make sign', fg_color='#C57A44', command=lambda : EncodeFile(self.encode_file_path, self.private_key))
         self.sign_file = ctk.CTkButton(self.main_frame, corner_radius=0, text = 'Make sign', fg_color='#C57A44', 
-                                       command=lambda : ServerMeet(self.client, f"send_encode_file {self.encode_file_path}"))
+                                       command=lambda : ServerMeet(self.client, f'encode_file {basename(self.encode_file_path)}'))
         
         
-        self.button_file.grid(row=1, column = 1, sticky = 'ew', padx=10, pady=5)
-        self.privateKey_file.grid(row=2, column = 1, sticky = 'ew', padx=10, pady=5)
-        self.sign_file.grid(row=3, column=1, sticky = 'ew', padx=10, pady=5)
+        self.button_file.grid(row=1, column = 2, sticky = 'nsew', padx=10, pady=5)
+        self.privateKey_file.grid(row=2, column = 2, sticky = 'ew', padx=10, pady=5)
+        self.sign_file.grid(row=3, column=2, sticky = 'ew', padx=10, pady=5)
 
 
    # def pick_and_send(self, name):
     #    if (name == "private_key"):
-
 
     def verify_main(self):
         for widget in self.main_frame.winfo_children():
@@ -117,11 +130,16 @@ class App(ctk.CTk):
         self.label = ctk.CTkLabel(self.main_frame, corner_radius=0, text='Выберите подписанный файл, исходный файл и открытый ключ:', 
                                   image=self.lablekey_image, compound='top', anchor=ctk.CENTER,font=ctk.CTkFont(size=20, weight="bold"))
         self.label.grid(row=0, column=1)
-
-        self.publicKey_file = ctk.CTkButton(self.main_frame, corner_radius=0, text = 'Public key', fg_color='#C57A44', command=lambda: self.pick_file(('Pem Files', '*.pem'), 'public_key'))
-        self.signed_file = ctk.CTkButton(self.main_frame, corner_radius=0, text = 'Signed file', fg_color='#C57A44', command=lambda: self.pick_file(('All files', '*.*'), 'signed_file'))
-        self.source_file = ctk.CTkButton(self.main_frame, corner_radius=0, text = 'Source file', fg_color='#C57A44', command=lambda: self.pick_file(('All files', '*.*'), 'source_file'))
-        self.verify_file = ctk.CTkButton(self.main_frame, corner_radius=0, text = 'Check sign', fg_color='#C57A44', command=lambda : 
+        self.label.rowconfigure(0, weight=1)
+        self.label.columnconfigure(1, weight=1)
+        self.publicKey_file = ctk.CTkButton(self.main_frame, corner_radius=0, text = 'Public key', fg_color='#C57A44', 
+                                            command=lambda: self.pick_file(('Pem Files', '*.pem'), 'public_key'))
+        self.signed_file = ctk.CTkButton(self.main_frame, corner_radius=0, text = 'Signed file', fg_color='#C57A44', 
+                                         command=lambda: self.pick_file(('All files', '*.*'), 'signed_file'))
+        self.source_file = ctk.CTkButton(self.main_frame, corner_radius=0, text = 'Source file', fg_color='#C57A44', 
+                                         command=lambda: self.pick_file(('All files', '*.*'), 'source_file'))
+        self.verify_file = ctk.CTkButton(self.main_frame, corner_radius=0, text = 'Check sign', fg_color='#C57A44', 
+                                         command=lambda : 
                             self.VerifyFile_(path_sig = self.verify_file_path, path_source=self.source_file, public_key=self.public_key))
         
         
@@ -132,37 +150,47 @@ class App(ctk.CTk):
 
         
     def VerifyFile_(self, path_sig, path_source, public_key):
-        if VerifyFile(path_sig=path_sig, path_source=path_source, public_key=public_key) == True:
+        res = ServerMeet(self.client, f"verify_sign {basename(path_sig)} {basename(path_source)}")
+        #print(res, res == True)
+        if res == 'True':
             self.label.configure(image = self.lableverify_image2)
         else:
             self.label.configure(image = self.lableverify_image1)
+       # else:
+        #    self.label.configure(image = self.lableverify_image1)
+            
             
 
 
 
     
-        
+         
     def pick_file(self, format, name):
         filepath = ctk.filedialog.askopenfilename(filetypes=[format])
 
         if name == 'private_key':
             self.private_key = useKey(filepath)
             #self.client.send('use_key ')
-            ServerMeet(self.client, f'use_key {filepath}')
+            ServerMeet(self.client, f'use_key {(filepath)}')
             #self.public_key = self.private_key.verifying_key
             if self.private_key != None:
                 print('Readed key')
         elif name == 'public_key':
             self.public_key = UsePub(filepath)
+            ServerMeet(self.client, f'use_key_pub {(filepath)}')
+            
         elif name == 'encode_file_path':
             self.encode_file_path = filepath
             ServerMeet(self.client, f'send_file {filepath}')
-
+            #self.SendFile_()
 
         elif name == 'signed_file':
             self.verify_file_path = filepath
+            ServerMeet(self.client, f'send_file {filepath}')
+            
         elif name == 'source_file':
             self.source_file = filepath
+            ServerMeet(self.client, f'send_file {filepath}')
         
 
         print(filepath)
